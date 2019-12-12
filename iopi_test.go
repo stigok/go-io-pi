6232f59f353886d2ecb13160d2e1074f0d27aa3e
Stig Otnes Kolstad <stig@stigok.com>
2019-12-13 00:22:16 +0100

    test: add test for all functions except ReadPin

--- iopi_test.go
+++ iopi_test.go
@@ -186,3 +186,176 @@ func TestSetPinPolarity(t *testing.T) {
 		}
 	})
 }
+
+func TestSetPortDirection(t *testing.T) {
+	t.Run("port A", func (t *testing.T) {
+		file := NewFakeFile()
+		dev := NewDevice(file, 0x20)
+
+		dev.SetPortDirection(PortA, 0x55)
+		if !file.HasCall("Write", []byte{ IODIRA, 0x55}) {
+			t.Error("did not write expected data", file.CallHistory)
+		}
+	})
+
+	t.Run("port B", func (t *testing.T) {
+		file := NewFakeFile()
+		dev := NewDevice(file, 0x20)
+
+		dev.SetPortDirection(PortB, 0x55)
+		if !file.HasCall("Write", []byte{ IODIRB, 0x55}) {
+			t.Error("did not write expected data", file.CallHistory)
+		}
+	})
+}
+
+func TestSetPinDirection(t *testing.T) {
+	t.Run("pin number < 8", func (t *testing.T) {
+		file := NewFakeFile()
+		dev := NewDevice(file, 0x20)
+
+		file.NextRead = []byte{ 0x00, 0x00 }
+		dev.SetPinDirection(7, 1)
+
+		if !file.HasCall("Write", []byte{ IODIRA, 0b01000000 }) {
+			t.Error("did not write expected data", file.CallHistory)
+		}
+	})
+
+	t.Run("pin number > 8", func (t *testing.T) {
+		file := NewFakeFile()
+		dev := NewDevice(file, 0x20)
+
+		file.NextRead = []byte{ 0x00, 0x00 }
+		dev.SetPinDirection(16, 1)
+
+		if !file.HasCall("Write", []byte{ IODIRB, 0b10000000 }) {
+			t.Error("did not write expected data", file.CallHistory)
+		}
+	})
+}
+
+func TestWritePort(t *testing.T) {
+	t.Run("port A", func (t *testing.T) {
+		file := NewFakeFile()
+		dev := NewDevice(file, 0x20)
+
+		dev.WritePort(PortA, 0xFF)
+
+		if !file.HasCall("Write", []byte{ GPIOA, 0xFF}) {
+			t.Error("did not write expected data", file.CallHistory)
+		}
+	})
+
+	t.Run("port B", func (t *testing.T) {
+		file := NewFakeFile()
+		dev := NewDevice(file, 0x20)
+
+		dev.WritePort(PortB, 0xFF)
+
+		if !file.HasCall("Write", []byte{ GPIOB, 0xFF}) {
+			t.Error("did not write expected data", file.CallHistory)
+		}
+	})
+}
+
+func TestReadPort(t *testing.T) {
+	t.Run("port A", func (t *testing.T) {
+		file := NewFakeFile()
+		dev := NewDevice(file, 0x20)
+
+		dev.ReadPort(PortA)
+
+		if !file.HasCall("Read", []byte{ GPIOA }) {
+			t.Error("did not write expected data", file.CallHistory)
+		}
+	})
+
+	t.Run("port B", func (t *testing.T) {
+		file := NewFakeFile()
+		dev := NewDevice(file, 0x20)
+
+		dev.ReadPort(PortB)
+
+		if !file.HasCall("Write", []byte{ GPIOB }) {
+			t.Error("did not write expected data", file.CallHistory)
+		}
+	})
+}
+
+func TestWritePin(t *testing.T) {
+	t.Run("pin <= 8", func (t *testing.T) {
+		file := NewFakeFile()
+		dev := NewDevice(file, 0x20)
+
+		file.NextRead = []byte{ 0x00, 0x00 }
+		dev.WritePin(7, High)
+
+		if !file.HasCall("Write", []byte{ GPIOA, 0b01000000 }) {
+			t.Error("did not write expected data", file.CallHistory)
+		}
+	})
+
+	t.Run("pin > 8", func (t *testing.T) {
+		file := NewFakeFile()
+		dev := NewDevice(file, 0x20)
+
+		file.NextRead = []byte{ 0x00, 0x00 }
+		dev.WritePin(15, High)
+
+		if !file.HasCall("Write", []byte{ GPIOB, 0b01000000 }) {
+			t.Error("did not write expected data", file.CallHistory)
+		}
+	})
+}
+
+func TestTranslatePin(t *testing.T) {
+	t.Run("pin <= 8", func (t *testing.T) {
+		pin, port := translatePin(7)
+		if pin != 7 - 1 {
+			t.Error("pin number off by one")
+		}
+		if port != PortA {
+			t.Error("wrong port returned")
+		}
+	})
+
+	t.Run("pin > 8", func (t *testing.T) {
+		pin, port := translatePin(16)
+		if pin != 8 - 1 {
+			t.Error("pin number off by one")
+		}
+		if port != PortB {
+			t.Error("wrong port returned")
+		}
+	})
+}
+
+
+func TestReadPin(t *testing.T) {
+	// I don't know how to test this yet
+	t.Run("pin <= 8", func (t *testing.T) {
+		t.Skip()
+	})
+
+	t.Run("pin > 8", func (t *testing.T) {
+		t.Skip()
+	})
+}
+
+func TestSetBit(t *testing.T) {
+	var b byte = 0b00000000
+	b = setBit(b, 3, 1)
+
+	if b != 0b00001000 {
+		t.Error("expected bit was not set")
+	}
+}
+
+func TestGetBit(t *testing.T) {
+	var b byte = 0b01000000
+	bit := getBit(b, 6)
+	if bit != 1 {
+		t.Error("expected bit was not set")
+	}
+}
